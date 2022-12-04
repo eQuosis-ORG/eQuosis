bda9dce12919071d56a9d25933189884ad3e83ef
Arvin661 <83867335+Arvin661@users.noreply.github.com>
2022-12-04 18:44:00 +0400

    Close - opening Projects

--- JS/main.js
+++ JS/main.js
@@ -23,9 +23,43 @@ downarrow.addEventListener("click", () => {
   downarrow.classList.toggle("drop-down_normal");
 });
 
+const closebtn = document.querySelector("#close-btn");
+const informationpan = document.getElementById("information");
+
+closebtn.addEventListener("click", () => {
+  informationpan.classList.toggle("opacity-0");
+  informationpan.classList.toggle("inform-animate");
+  tile.classList.toggle("tile-animate-none");
+});
+
+const equosistab = document.querySelector(".main-website");
+const tile = document.getElementById("tile");
+
+equosistab.addEventListener("click", () => {
+  informationpan.classList.toggle("opacity-0");
+  informationpan.classList.toggle("inform-animate");
+  tile.classList.toggle("tile-animate-none");
+});
+
 gsap.from("#title", {
   scrollTrigger: {
     scrub: true,
   },
   y: 500,
 });
+
+function sendEmail() {
+  Email.send({
+    secureToken: "268ea5ea-c4b4-495e-b6aa-85d7246978ae",
+    To: "equosiscoders@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Email from Contact Form",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Query: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message sent"));
+}
